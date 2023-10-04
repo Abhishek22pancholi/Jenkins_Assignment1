5795b4e39276ee8ad3db1720dbefe15c4bc5377c
Abhishek22pancholi <abhishekpancholi22@gmail.com>
2023-10-04 18:59:24 +0530

    Jenkins Assignment Bench

---
+++ src/test/java/jenkinsMavenIntegration/SampleTestCase.java
@@ -0,0 +1,46 @@
+package jenkinsMavenIntegration;
+
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class SampleTestCase {
+	
+	
+	@Parameters("browser")
+	@Test
+	public void lauchBrowser(@Optional("Chrome")String Launching_browser)
+	{
+		WebDriver driver=null;
+		System.out.println("Launch browser is : " + Launching_browser);
+		
+		if(Launching_browser.contains("Chrome"))
+		{
+		
+		System.setProperty("webdriver.chrome.driver","C:\\WORKPLACE\\Jenkins\\BenchTraining\\BenchTraining\\chromedriver.exe");
+
+		driver = new ChromeDriver();
+		}
+		
+		else if(Launching_browser.contains("Firefox"))
+		{System.setProperty("webdriver.chrome.driver","C:\\WORKPLACE\\Jenkins\\BenchTraining\\BenchTraining\\chromedriver.exe");
+
+		driver = new ChromeDriver();
+			
+		}
+		
+		driver.get("https://www.google.com/");
+		
+		System.out.println(driver.getTitle());
+		
+		Assert.assertTrue(driver.getTitle().contains("Google"),"Title match");
+		
+		driver.quit();
+	}
+
+}
